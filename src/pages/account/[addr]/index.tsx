092fa47ca69b1d785091e5d4649678dab54705e1
loicnico96 <loicnico96@gmail.com>
2021-11-16 15:37:41 +0100

    Send asset

--- src/pages/account/[addr]/index.tsx
+++ src/pages/account/[addr]/index.tsx
@@ -103,6 +103,158 @@ export default function ViewAccountPage() {
     )
   }, [address])
 
+  const onSendAsset = useCallback(async () => {
+    if (!address) {
+      return
+    }
+
+    // eslint-disable-next-line no-alert
+    const assetId = window.prompt("Asset ID:")
+    if (assetId === null) {
+      return
+    }
+
+    if (!Number.isInteger(Number(assetId)) || Number(assetId) < 0) {
+      throw Error("Invalid asset ID")
+    }
+
+    // eslint-disable-next-line no-alert
+    const to = window.prompt("Receiver address:")
+    if (to === null) {
+      return
+    }
+
+    if (!algosdk.isValidAddress(to)) {
+      throw Error("Invalid address")
+    }
+
+    // eslint-disable-next-line no-alert
+    const amountStr = window.prompt("Amount:")
+    if (amountStr === null) {
+      return
+    }
+
+    const amount = Number(amountStr)
+
+    if (!Number.isFinite(amount)) {
+      throw Error("Invalid address")
+    }
+
+    // eslint-disable-next-line no-alert
+    const note = window.prompt("Note (optional):")
+
+    const suggestedParams = await AlgoAPI.getTransactionParams().do()
+
+    const transaction =
+      algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject({
+        amount,
+        assetIndex: Number(assetId),
+        from: address,
+        note: note ? new TextEncoder().encode(note) : undefined,
+        suggestedParams,
+        to,
+      })
+
+    let signed: Uint8Array | undefined
+
+    withSecretKey(key => {
+      signed = transaction.signTxn(key)
+    })
+
+    if (!signed) {
+      return
+    }
+
+    await AlgoAPI.sendRawTransaction(signed).do()
+
+    // eslint-disable-next-line no-alert
+    window.alert(
+      `Sending ${amount} of asset ${assetId} to ${to}...\nTransaction ID: ${transaction.txID()}`
+    )
+  }, [address])
+
+  const addApplication = useCallback(async () => {
+    if (!address) {
+      return
+    }
+
+    // eslint-disable-next-line no-alert
+    const appId = window.prompt("Application ID:")
+    if (appId === null) {
+      return
+    }
+
+    if (!Number.isInteger(Number(appId)) || Number(appId) < 0) {
+      throw Error("Invalid application ID")
+    }
+
+    const suggestedParams = await AlgoAPI.getTransactionParams().do()
+
+    const transaction = algosdk.makeApplicationOptInTxnFromObject({
+      appIndex: Number(appId),
+      from: address,
+      suggestedParams,
+    })
+
+    let signed: Uint8Array | undefined
+
+    withSecretKey(key => {
+      signed = transaction.signTxn(key)
+    })
+
+    if (!signed) {
+      return
+    }
+
+    await AlgoAPI.sendRawTransaction(signed).do()
+
+    // eslint-disable-next-line no-alert
+    window.alert(
+      `Adding application ${appId}...\nTransaction ID: ${transaction.txID()}`
+    )
+  }, [address])
+
+  const removeApplication = useCallback(async () => {
+    if (!address) {
+      return
+    }
+
+    // eslint-disable-next-line no-alert
+    const appId = window.prompt("Application ID:")
+    if (appId === null) {
+      return
+    }
+
+    if (!Number.isInteger(Number(appId)) || Number(appId) < 0) {
+      throw Error("Invalid application ID")
+    }
+
+    const suggestedParams = await AlgoAPI.getTransactionParams().do()
+
+    const transaction = algosdk.makeApplicationCloseOutTxnFromObject({
+      appIndex: Number(appId),
+      from: address,
+      suggestedParams,
+    })
+
+    let signed: Uint8Array | undefined
+
+    withSecretKey(key => {
+      signed = transaction.signTxn(key)
+    })
+
+    if (!signed) {
+      return
+    }
+
+    await AlgoAPI.sendRawTransaction(signed).do()
+
+    // eslint-disable-next-line no-alert
+    window.alert(
+      `Removing application ${appId}...\nTransaction ID: ${transaction.txID()}`
+    )
+  }, [address])
+
   const addAsset = useCallback(async () => {
     if (!address) {
       return
@@ -165,9 +317,18 @@ export default function ViewAccountPage() {
         <button onClick={onRemoveAccount}>Remove account</button>
       )}
       {address === getAddress() && (
+        <button onClick={addApplication}>Add application</button>
+      )}
+      {address === getAddress() && (
+        <button onClick={removeApplication}>Remove application</button>
+      )}
+      {address === getAddress() && (
         <button onClick={addAsset}>Add asset</button>
       )}
       {address === getAddress() && <button onClick={onSend}>Send Algos</button>}
+      {address === getAddress() && (
+        <button onClick={onSendAsset}>Send asset</button>
+      )}
     </div>
   )
 }
