a8f43fca51d082ef76e168ebfb152ed1f1f5fc1d
loicnico96 <loicnico96@gmail.com>
2021-11-16 12:33:05 +0100

    Payment transaction

--- src/pages/account/[addr]/index.tsx
+++ src/pages/account/[addr]/index.tsx
@@ -3,7 +3,9 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 import { useCallback, useEffect } from "react"
 
+import { ALGO_ASSET_DECIMALS } from "components/NativeAsset"
 import ViewAccount from "components/ViewAccount"
+import { AlgoAPI } from "lib/algo/AlgoAPI"
 import {
   changePIN,
   getAddress,
@@ -41,6 +43,66 @@ export default function ViewAccountPage() {
     }
   }, [router])
 
+  const onSend = useCallback(async () => {
+    if (!address) {
+      return
+    }
+
+    // eslint-disable-next-line no-alert
+    const to = window.prompt("Receiver address:")
+    if (to === null) {
+      return
+    }
+
+    if (!algosdk.isValidAddress(to)) {
+      throw Error("Invalid address")
+    }
+
+    // eslint-disable-next-line no-alert
+    const amountStr = window.prompt("Amount (Algos):")
+    if (amountStr === null) {
+      return
+    }
+
+    const amount = Number(amountStr)
+
+    if (!Number.isFinite(amount)) {
+      throw Error("Invalid address")
+    }
+
+    // eslint-disable-next-line no-alert
+    const note = window.prompt("Note (optional):")
+
+    const suggestedParams = await AlgoAPI.getTransactionParams().do()
+
+    const transaction = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+      amount: Math.ceil(amount * 10 ** ALGO_ASSET_DECIMALS),
+      from: address,
+      note: note ? new TextEncoder().encode(note) : undefined,
+      suggestedParams,
+      to,
+    })
+
+    let signed: Uint8Array | undefined
+
+    withSecretKey(key => {
+      signed = transaction.signTxn(key)
+    })
+
+    if (!signed) {
+      return
+    }
+
+    await AlgoAPI.sendRawTransaction(signed).do()
+
+    // eslint-disable-next-line no-alert
+    window.alert(
+      `Sending ${Math.ceil(
+        amount * 10 ** ALGO_ASSET_DECIMALS
+      )} Algos to ${to}...\nTransaction ID: ${transaction.txID()}`
+    )
+  }, [address])
+
   if (!address) {
     return <pre>Loading...</pre>
   }
@@ -58,6 +120,7 @@ export default function ViewAccountPage() {
       {address === getAddress() && (
         <button onClick={onRemoveAccount}>Remove account</button>
       )}
+      {address === getAddress() && <button onClick={onSend}>Send Algos</button>}
     </div>
   )
 }
