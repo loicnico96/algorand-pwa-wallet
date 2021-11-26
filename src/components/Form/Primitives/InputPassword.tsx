1783ccc0c04c5caf6bc16d8f1184e02a79ce7731
loicnico96 <loicnico96@gmail.com>
2021-11-26 08:22:06 +0100

    Disable autocomplete on password input

--- src/components/Form/Primitives/InputPassword.tsx
+++ src/components/Form/Primitives/InputPassword.tsx
@@ -3,5 +3,5 @@ import { InputBase, InputBaseProps } from "./InputBase"
 export type InputPasswordProps = Omit<InputBaseProps, "type">
 
 export function InputPassword(props: InputPasswordProps) {
-  return <InputBase {...props} type="password" />
+  return <InputBase {...props} autoComplete="off" type="password" />
 }
